d629c91b8c62a7a9ba7047de1fd132893933c490
cgyee <cyee2017@fau.edu>
2021-05-02 22:40:18 -0400

    added the skeleton code for liking posts, updates in database but not reflected in front end ui

--- controllers/feed.js
+++ controllers/feed.js
@@ -16,6 +16,7 @@ module.exports = {
         try {
             const uid = req.user.id
             const postId = req.body.postId
+            //get the post with the matching id the add it to array of liked posts in the user model
             const post = await Post.find({_id: postId})
             const result =  await User.updateOne({_id:uid}, {likedPosts:post})
             // const result = await User.find({_id: uid, "likedPosts._id":postId})
