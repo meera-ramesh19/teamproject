3b771e5cce518e1dcf382cab70e5f351cc0fbb13
D'yara Williams <dyara.williams@gmail.com>
2021-05-03 20:10:07 -0400

    fixed feed controller for posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,7 +4,7 @@ module.exports = {
     getPosts: async(req, res) => {
         // res.render('feed.ejs')
         try {
-            const postItems = await Post.find({ userId:'608af98c89c2bf71b3cde994' })
+            const postItems = await Post.find().sort({ postDate: "desc" }).lean();
             console.log(postItems)
             res.render('feed.ejs', { posts: postItems, user: req.user })
         } catch (err) {
