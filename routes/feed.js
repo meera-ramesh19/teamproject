c768e20b01ebee85bb40ab3dc429d1672b8dc7e3
cgyee <cyee2017@fau.edu>
2021-05-06 21:02:01 -0400

    implementing liked posts feature

--- routes/feed.js
+++ routes/feed.js
@@ -4,6 +4,7 @@ const feedController = require('../controllers/feed')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', ensureAuth, feedController.getPosts)
+router.get('/likedPosts', feedController.getLikedPosts)
 router.put('/addLike', feedController.addLike)
 
 module.exports = router
