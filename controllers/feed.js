0f3f1722c0c40c5258f6fe61a23bbfad331b883d
Yolanda Haynes <yhaynes@giyo.us>
2021-05-06 17:29:14 -0700

    modified feed.js and feed.ejs

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,10 +2,8 @@ const Post = require('../models/Post')
 
 module.exports = {
     getPosts: async(req, res) => {
-        // res.render('feed.ejs')
         try {
             const postItems = await Post.find()
-            // console.log(postItems)
             res.render('feed.ejs', { posts: postItems, user: req.user })
         } catch (err) {
             console.log(err)
